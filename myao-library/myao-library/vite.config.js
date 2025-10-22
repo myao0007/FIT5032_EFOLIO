e56a64c11dff2ec0d6730765dd4ccc9517a91acb
myao0007 <myao0007@student.monash.edu>
2025-10-22 22:19:14 +1100

    fix deploy for subfolder project

--- myao-library/myao-library/vite.config.js
+++ myao-library/myao-library/vite.config.js
@@ -15,7 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production'
-    ? '/FIT5032_EFOLIO/'
-    : '/'
+  base: process.env.NODE_ENV === 'production' ? '/FIT5032_EFOLIO/' : '/'
 })
